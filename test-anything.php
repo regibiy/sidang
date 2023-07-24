4d02b52fd87093899a4f1efdb65a02a39c76b515
Regi Ridho Biyantomo <regibiyantomo123@gmail.com>
2023-07-25 05:10:55 +0700

    delete assets and add test-any

--- test-anything.php
+++ test-anything.php
@@ -1,38 +1,75 @@
+<input type="text" maxlength="6" id="inputsatu">
+<br>
+<input type="text" maxlength="10" id="inputdua" value="2023">
+
+<script>
+    const inputsatu = document.getElementById("inputsatu");
+    const inputdua = document.getElementById("inputdua");
+    let valueDua = inputdua.value;
+    let a, hasilGabung;
+    inputsatu.addEventListener("input", () => {
+        a = inputsatu.value;
+        hasilGabung = valueDua + a.substring(1, 6);
+        inputdua.value = hasilGabung;
+    })
+</script>
+
+
+<!-- <div style="display:flex;justify-content:center;align-items:center;">
+    <a href='https://puskesmasalianyangpnk.site' style='text-decoration:none;
+color:white;
+padding:0.5rem;
+background-color:blue;
+width:150px;
+border-radius:6px'>
+        Verifikasi Email Anda</a>
+</div> -->
 <?php
+include("action.php");
 
-$tanggal = "2023-06-29";
+$content = "
+Hai, Regi Ridho Biyantomo<br><br><br>
+Terima kasih telah melakukan pendaftaran di UPT Puskesmas Alianyang. Agar proses pendaftaran akun Anda dapat diproses oleh petugas kami.
+Silakan klik tombol Verifikasi Email di bawah ini untuk memverifikasi bahwa benar ini adalah Anda.
+<br><br><br>
+<a href='https://puskesmasalianyangpnk.site' style='text-decoration:none;color:white;padding:0.5rem;background-color:blue;width:100px;'>Verifikasi Email Anda</a>
+";
 
-function cek_libur_nasional($tanggal)
-{
-    $timestamp = strtotime($tanggal);
-    $month = date('n', $timestamp); //ambil bulan tanpa leading zero
-    $default_date = date("Y-m-j", $timestamp); //ambil hari tanpa leading zero
-    $url = "https://api-harilibur.vercel.app/api?month=$month";
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); //return string
-    try {
-        $data = curl_exec($curl);
-        if ($data === false) {
-            throw new Exception(curl_error($curl));
-        }
-        $array_data = json_decode($data);
-        foreach($array_data as $value) {
-            if ($default_date === $value->holiday_date) {
-                $national_holiday = $value->holiday_name;
-                return $national_holiday;
-            }
-        }
-    } catch (Exception $e) {
-        echo 'Error: ' . $e->getMessage();
-        die;
-    }
-    curl_close($curl);
-}
+// sendMail("regibiyantomo123@gmail.com", "regi", "test", $content);
 
-$hai = cek_libur_nasional($tanggal);
-var_dump($hai);
-die;
+// $tanggal = "2023-06-29";
+
+// function cek_libur_nasional($tanggal)
+// {
+//     $timestamp = strtotime($tanggal);
+//     $month = date('n', $timestamp); //ambil bulan tanpa leading zero
+//     $default_date = date("Y-m-j", $timestamp); //ambil hari tanpa leading zero
+//     $url = "https://api-harilibur.vercel.app/api?month=$month";
+//     $curl = curl_init();
+//     curl_setopt($curl, CURLOPT_URL, $url);
+//     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); //return string
+//     try {
+//         $data = curl_exec($curl);
+//         if ($data === false) {
+//             throw new Exception(curl_error($curl));
+//         }
+//         $array_data = json_decode($data);
+//         foreach ($array_data as $value) {
+//             if ($default_date === $value->holiday_date) {
+//                 $national_holiday = $value->holiday_name;
+//                 return $national_holiday;
+//             }
+//         }
+//     } catch (Exception $e) {
+//         echo 'Error: ' . $e->getMessage();
+//         die;
+//     }
+//     curl_close($curl);
+// }
+
+// $hai = cek_libur_nasional($tanggal);
+// var_dump($hai);
+// die;
 
 // ----
 
@@ -63,55 +100,6 @@ die;
 // curl_close($curl);
 
 // -----
-
-// include("crypt.php");
-
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\SMTP;
-// use PHPMailer\PHPMailer\Exception;
-
-// function sendMail($emailReceiver, $nameReceiver, $title, $content)
-// {
-//     $emailSender = "noreply@puskesmasalianyangpnk.my.id";
-//     $senderName = "Puskesmas Alianyang";
-    
-//     require 'vendor/autoload.php';
-
-//     $mail = new PHPMailer(true);
-
-//     try {
-//         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                                      
-//         $mail->isSMTP();                                            
-//         $mail->Host       = 'puskesmasalianyangpnk.my.id';              
-//         $mail->SMTPAuth   = true;                                
-//         $mail->Username   = $emailSender;                        
-//         $mail->Password   = "okMgx%M5sE6N";                
-//         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         
-//         $mail->Port       = 465;                            
-
-//         $mail->setFrom($emailSender, $senderName);
-//         $mail->addAddress($emailReceiver, $nameReceiver);          
-
-//         $mail->isHTML(true);
-//         $mail->Subject = $title;
-//         $mail->Body    = $content;
-
-//         $mail->send();
-//         echo 'Message has been sent';
-//     } catch (Exception $e) {
-//         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-//     }
-// }
-
-
-// $email = "regibiyantomo123@gmail.com";
-// $nama_lengkap = "regi";
-// $title = "hai";
-// $content = "haha";
-// sendMail($email, $nama_lengkap, $title, $content);
-
-// -----
-
 // function cek_libur_nasional($tanggal)
 // {
 //     $timestamp = strtotime($tanggal);
